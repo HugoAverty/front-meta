d4f7e508d577925e98ad17544eb116b6ba9b78cd
Alexis Faizeau <a.faizeau@workadventu.re>
2022-05-04 15:28:52 +0200

    Add unselect on faq

--- js/nft.js
+++ js/nft.js
@@ -11,7 +11,7 @@ const eltNFT = document.querySelector('#elt-nft')
 const elFAQ = document.querySelector('#elt-faq')
 const elTeam = document.querySelector('#elt-team')
 const observer = new window.IntersectionObserver(([entry]) => {
-    var menu = document.querySelector('.'+entry.target.id);
+    var menu = document.querySelector('.' + entry.target.id);
     if (entry.isIntersecting) {
         const menuItems = document.querySelectorAll('.menu-item');
         menuItems.forEach(item => {
@@ -29,7 +29,7 @@ const observer = new window.IntersectionObserver(([entry]) => {
 
 var menuItem = document.querySelectorAll(".menu-item");
 for (const item of menuItem) {
-    item.addEventListener('click', function(event) {
+    item.addEventListener('click', function (event) {
         this.classList.add("activeMenu");
     })
 }
@@ -51,11 +51,21 @@ gsap.to(".bg-pattern", {
 });
 
 
-window.onload = function() {
+window.onload = function () {
     var tlIntro = new TimelineMax();
-    tlIntro.to(".block.block-intro.block-intro-nft .bg-block-intro-nft", 30, {scale: 1.3}, 0);
+    tlIntro.to(".block.block-intro.block-intro-nft .bg-block-intro-nft", 30, { scale: 1.3 }, 0);
 
     observer.observe(elTeam);
     observer.observe(elFAQ);
     observer.observe(eltNFT);
 }
+
+elFAQ.querySelectorAll("label").forEach((item) => {
+    item.addEventListener('click', (event) => {
+        const currentInput = event.currentTarget.parentNode.querySelector('input[name="toggle"]');
+        if (currentInput && currentInput.checked) {
+            currentInput.checked = false;
+            event.preventDefault();
+        }
+    });
+});
