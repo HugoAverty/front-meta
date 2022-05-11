cf9c3a6dc67710d642f07c8e450f2c38d11579e2
Gregoire Parant <g.parant@thecodingmachine.com>
2022-05-11 22:47:57 +0200

    Create animated NFT Woka

--- js/nft.js
+++ js/nft.js
@@ -58,6 +58,16 @@ window.onload = function () {
     observer.observe(elTeam);
     observer.observe(elFAQ);
     observer.observe(eltNFT);
+
+    wokaCard('gen1');
+    wokaCard('gen2');
+    wokaCard('gen3');
+    wokaCard('gen4');
+    wokaCard('gen5');
+    wokaCard('gen6');
+    wokaCard('gen7');
+    wokaCard('gen8');
+    wokaCard('gen9');
 }
 
 elFAQ.querySelectorAll("label").forEach((item) => {
@@ -69,3 +79,106 @@ elFAQ.querySelectorAll("label").forEach((item) => {
         }
     });
 });
+
+const animatedTextures = [];
+
+function wokaCard(id){
+    //cancel animation WOKA
+    if(animatedTextures[id] != undefined){
+        window.cancelAnimationFrame(animatedTextures[id].requestAnimationFrame);
+    }
+
+    //create canvas animation
+    const image = new Image();
+    image.src = `img/NFT/static/${id}.png`;
+    const canvas = document.getElementById(id);
+    const animatedTexture = {
+        image: image,
+        canvas: canvas,
+        context: canvas.getContext('2d'),
+        cycleLoop: [0, 1, 0, 2],
+        currentLoopIndex: 0,
+        frameCount: 0,
+        currentDirection: 0,
+        scale: 2,
+        width: 32,
+        height: 32,
+        requestAnimationFrame: null,
+    };
+    animatedTexture.context.imageSmoothingEnabled = false;
+    animatedTexture.requestAnimationFrame = window.requestAnimationFrame(() => {
+        stepAnimatedTexture(id);
+    });
+
+    animatedTextures[id] = animatedTexture;
+
+    //step animation
+    stepAnimatedTexture(id);
+}
+
+const stepAnimatedTexture = (id) => {
+    const animatedTexture = animatedTextures[id];
+
+    // Go to the next texture each 15 frames
+    animatedTexture.frameCount++;
+    if (animatedTexture.frameCount < 35) {
+        animatedTexture.requestAnimationFrame = window.requestAnimationFrame(() => {
+            stepAnimatedTexture(id); 
+        });
+        return;
+    }
+    animatedTexture.frameCount = 0;
+
+    // Clear the canvas
+    clearCanvasAnimatedTexture(id);
+
+    // Draw texture
+    animatedTextureDrawFrame(
+        id,
+        animatedTexture.cycleLoop[animatedTexture.currentLoopIndex],
+        animatedTexture.currentDirection,
+        0,
+        0
+    );
+
+    // Check the next texture
+    animatedTexture.currentLoopIndex++;
+    if (animatedTexture.currentLoopIndex >= animatedTexture.cycleLoop.length) {
+        animatedTexture.currentLoopIndex = 0;
+        animatedTexture.currentDirection++;
+    }
+
+    // Reset the Woka position
+    if (animatedTexture.currentDirection >= 4) {
+        animatedTexture.currentDirection = 0;
+    }
+
+    animatedTexture.requestAnimationFrame = window.requestAnimationFrame(() => {
+        stepAnimatedTexture(id)
+    });
+}
+
+const  animatedTextureDrawFrame = (id, frameX, frameY, canvasX, canvasY) => {
+    const animatedTexture = animatedTextures[id];
+    animatedTexture.context.drawImage(
+        animatedTexture.image,
+        frameX * animatedTexture.width,
+        frameY * animatedTexture.height,
+        animatedTexture.width,
+        animatedTexture.height,
+        canvasX,
+        canvasY,
+        animatedTexture.scale * animatedTexture.width,
+        animatedTexture.scale * animatedTexture.height
+    );
+}
+
+const clearCanvasAnimatedTexture = (id) => {
+    const animatedTexture = animatedTextures[id];
+    animatedTexture.context.clearRect(
+        0,
+        0,
+        animatedTexture.canvas.width,
+        animatedTexture.canvas.height
+    );
+}
